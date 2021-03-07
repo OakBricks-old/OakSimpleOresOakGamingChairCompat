d4813ee9553c53c3b596a7e7c992e0e263af2ea7
OakBricks <oakbricks@gmail.com>
2021-03-07 00:27:34 -0600

    FIrst Commit

---
+++ src/main/java/compact/oakbricks/simpleoreschairscompat/block/RefineryBlock.java
@@ -0,0 +1,49 @@
+
+package compact.oakbricks.simpleoreschairscompat.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.List;
+import java.util.Collections;
+
+import compact.oakbricks.simpleoreschairscompat.SimpleoreschairscompatModElements;
+
+@SimpleoreschairscompatModElements.ModElement.Tag
+public class RefineryBlock extends SimpleoreschairscompatModElements.ModElement {
+	@ObjectHolder("simpleoreschairscompat:refinery")
+	public static final Block block = null;
+	public RefineryBlock(SimpleoreschairscompatModElements instance) {
+		super(instance, 7);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			setRegistryName("refinery");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
